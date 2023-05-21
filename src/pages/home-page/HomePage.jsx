8cbbab262ea072b7bd53d30278d246547835b472
barisAkgul <barisakgul1994@gmail.com>
2023-05-21 21:59:04 +0300

    Added pages components

---
+++ src/pages/home-page/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { HomePageContainer } from "@containers/home-page-container/HomePageContainer";
+
+const HomePage = () => {
+  return <HomePageContainer />;
+};
+
+export { HomePage };
