89cbbaf9583cfc60cb33054db1a9bac3be9db7bf
barisAkgul <barisakgul1994@gmail.com>
2023-05-25 22:10:34 +0300

    Updated folder name

---
+++ src/components/header/icon-section/search-icon/SearchIcon.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Modal } from "antd";
+import { IoSearchOutline } from "react-icons/io5";
+
+import SearchSectionContainer from "@containers/search-section-container/SearchSectionContainer";
+import "./SearchIcon.scss";
+
+const SearchIcon = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <IoSearchOutline onClick={showModal} />
+
+      <Modal
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        width={720}
+        style={{ top: "50px" }}
+        footer={false}
+      >
+        <SearchSectionContainer />
+      </Modal>
+    </>
+  );
+};
+
+export default SearchIcon;
---
+++ src/containers/search-section-container/SearchSectionContainer.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { IoSearchOutline } from "react-icons/io5";
+
+import { useStore } from "@stores";
+
+import "./SearchSectionContainer.scss";
+
+import SearchProductCart from "@components/common/search-product-cart/SearchProductCart";
+import { filterProducts } from "@helpers/utils";
+
+const SearchSectionContainer = () => {
+  const { products } = useStore();
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const filteredProducts = filterProducts(products, searchValue);
+
+  console.log(filteredProducts, products);
+
+  return (
+    <>
+      <div className="search-section-top-area">
+        <h1 className="search-section-header">Search for products</h1>
+        <p className="search-section-sub-desc">
+          Start typing to see products you are looking for.
+        </p>
+      </div>
+      <div className="search-section-input-area">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="Search a product"
+            value={searchValue}
+            onChange={handleSearch}
+            className="search-input"
+          />
+          <button className="search-button">
+            <IoSearchOutline />
+          </button>
+        </form>
+      </div>
+
+      <div className="search-section-results-area">
+        <div className="search-results-heading">
+          <h3>RESULTS FROM PRODUCT</h3>
+        </div>
+        <div className="search-results">
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((product) => (
+              <SearchProductCart product={product} key={product.id} />
+            ))
+          ) : (
+            <a href="#" className="search-result-item">
+              😔Product Not Found😔
+            </a>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchSectionContainer;
