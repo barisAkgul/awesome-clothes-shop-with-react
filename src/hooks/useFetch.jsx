2d78e675d39d7ca8e83c3d53bdc4509263bc8ade
barisAkgul <barisakgul1994@gmail.com>
2023-05-26 15:57:50 +0300

    Added useFetchHooks

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, useRef } from "react";
+
+const useFetch = (requestCallback) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    const doFetch = async () => {
+      setLoading(true);
+      try {
+        const results = await requestCallback();
+        if (!signal.aborted) {
+          setData(results);
+        }
+      } catch (e) {
+        if (!signal.aborted) {
+          setError(e);
+        }
+      } finally {
+        if (!signal.aborted) {
+          setLoading(false);
+        }
+      }
+    };
+    doFetch();
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+  return { data, error, loading };
+};
+
+export default useFetch;
