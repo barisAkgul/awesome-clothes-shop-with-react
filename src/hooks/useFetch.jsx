9b4842851d65422fd897cd06f5fac3f656e85543
barisAkgul <barisakgul1994@gmail.com>
2023-06-07 22:45:20 +0300

    Added cache feature

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,3 +1,39 @@
+// import { useState, useEffect, useRef } from "react";
+
+// const useFetch = (requestCallback) => {
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true); // Başlangıçta loading durumu true olarak ayarlanır
+
+//   useEffect(() => {
+//     const abortController = new AbortController();
+//     const signal = abortController.signal;
+//     const doFetch = async () => {
+//       try {
+//         const results = await requestCallback();
+//         if (!signal.aborted) {
+//           setData(results);
+//         }
+//       } catch (e) {
+//         if (!signal.aborted) {
+//           setError(e);
+//         }
+//       } finally {
+//         if (!signal.aborted) {
+//           setLoading(false);
+//         }
+//       }
+//     };
+//     doFetch();
+//     return () => {
+//       abortController.abort();
+//     };
+//   }, []);
+//   return { data, error, loading };
+// };
+
+// export default useFetch;
+
 import { useState, useEffect, useRef } from "react";
 
 const useFetch = (requestCallback) => {
@@ -5,30 +41,43 @@ const useFetch = (requestCallback) => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true); // Başlangıçta loading durumu true olarak ayarlanır
 
+  const cacheData = useRef(null);
+
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
-    const doFetch = async () => {
-      try {
-        const results = await requestCallback();
-        if (!signal.aborted) {
-          setData(results);
-        }
-      } catch (e) {
-        if (!signal.aborted) {
-          setError(e);
-        }
-      } finally {
-        if (!signal.aborted) {
-          setLoading(false);
+
+    const fetchData = async () => {
+      if (cacheData.current) {
+        // Önbellekte veri varsa, onu kullan
+        setData(cacheData.current);
+        setLoading(false);
+      } else {
+        try {
+          const results = await requestCallback();
+          if (!signal.aborted) {
+            setData(results);
+            cacheData.current = results; // Verileri önbelleğe al
+          }
+        } catch (e) {
+          if (!signal.aborted) {
+            setError(e);
+          }
+        } finally {
+          if (!signal.aborted) {
+            setLoading(false);
+          }
         }
       }
     };
-    doFetch();
+
+    fetchData();
+
     return () => {
       abortController.abort();
     };
   }, []);
+
   return { data, error, loading };
 };
 
