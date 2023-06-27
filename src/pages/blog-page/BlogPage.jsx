2be789d8aed174addaab85283ff3842e24823b97
barisAkgul <barisakgul1994@gmail.com>
2023-06-27 21:12:16 +0300

    Added blog page component

---
+++ src/pages/blog-page/BlogPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BlogPageContainer } from "@containers/blog-page-container/BlogPAgeContainer";
+
+const BlogPage = () => {
+  return <BlogPageContainer />;
+};
+
+export { BlogPage };
