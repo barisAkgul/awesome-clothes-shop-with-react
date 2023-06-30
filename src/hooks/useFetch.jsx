f9cafa935007ffa5abb92b117800ae75b7a8e1ed
barisAkgul <barisakgul1994@gmail.com>
2023-06-30 17:37:59 +0300

    Remove comment lines

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,45 +1,9 @@
-// import { useState, useEffect, useRef } from "react";
-
-// const useFetch = (requestCallback) => {
-//   const [data, setData] = useState(null);
-//   const [error, setError] = useState(null);
-//   const [loading, setLoading] = useState(true); // Başlangıçta loading durumu true olarak ayarlanır
-
-//   useEffect(() => {
-//     const abortController = new AbortController();
-//     const signal = abortController.signal;
-//     const doFetch = async () => {
-//       try {
-//         const results = await requestCallback();
-//         if (!signal.aborted) {
-//           setData(results);
-//         }
-//       } catch (e) {
-//         if (!signal.aborted) {
-//           setError(e);
-//         }
-//       } finally {
-//         if (!signal.aborted) {
-//           setLoading(false);
-//         }
-//       }
-//     };
-//     doFetch();
-//     return () => {
-//       abortController.abort();
-//     };
-//   }, []);
-//   return { data, error, loading };
-// };
-
-// export default useFetch;
-
 import { useState, useEffect, useRef } from "react";
 
 const useFetch = (requestCallback) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true); // Başlangıçta loading durumu true olarak ayarlanır
+  const [loading, setLoading] = useState(true);
 
   const cacheData = useRef(null);
 
@@ -49,7 +13,7 @@ const useFetch = (requestCallback) => {
 
     const fetchData = async () => {
       if (cacheData.current) {
-        // Önbellekte veri varsa, onu kullan
+        // If there is data in the cache, use it
         setData(cacheData.current);
         setLoading(false);
       } else {
@@ -57,7 +21,7 @@ const useFetch = (requestCallback) => {
           const results = await requestCallback();
           if (!signal.aborted) {
             setData(results);
-            cacheData.current = results; // Verileri önbelleğe al
+            cacheData.current = results; // Cache the data
           }
         } catch (e) {
           if (!signal.aborted) {
